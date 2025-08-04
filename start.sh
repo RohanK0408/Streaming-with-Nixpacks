ad198706ca5e9565c54ef765377b2ed8a4ab32d8
RohanK0408 <129577063+RohanK0408@users.noreply.github.com>
2025-08-04 13:16:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
-
+#!/bin/bash
 echo "Starting livestream with FFmpeg..."
-ffmpeg -re -i "$SONG_URL" -f lavfi -i anullsrc -shortest -c:a aac -b:a 192k -f flv "$RTMP_URL/$STREAM_KEY"
+
+ffmpeg -re -i "$SONG_URL" \
+  -f lavfi -i anullsrc=r=44100:cl=stereo \
+  -shortest \
+  -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
+  -c:a aac -b:a 128k -ar 44100 \
+  -f flv "$RTMP_URL/$STREAM_KEY"
