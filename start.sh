9b4912e5126d2417572624b5440cf31b83024404
RohanK0408 <129577063+RohanK0408@users.noreply.github.com>
2025-08-04 13:25:38 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-echo "Starting livestream with FFmpeg..."
 
-ffmpeg -re -i "$SONG_URL" \
-  -f lavfi -i anullsrc=r=44100:cl=stereo \
-  -shortest \
+echo "Downloading from Google Drive..."
+gdown "$SONG_URL" -O video.mp4
+
+echo "Starting livestream with FFmpeg..."
+ffmpeg -re -i video.mp4 \
   -c:v libx264 -preset veryfast -maxrate 3000k -bufsize 6000k \
   -c:a aac -b:a 128k -ar 44100 \
   -f flv "$RTMP_URL/$STREAM_KEY"
